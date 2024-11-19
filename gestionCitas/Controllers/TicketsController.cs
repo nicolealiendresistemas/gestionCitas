023d8daba047ec2aab722b2d7247d06ec19b2be7
nicolealiendresistemas <nicolealiendresistemas@gmail.com>
2024-11-19 08:48:28 -0400

    cambios para ticket

---
+++ gestionCitas/Controllers/TicketsController.cs
@@ -0,0 +1,43 @@
+﻿using gestionCitas.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace gestionCitas.Controllers
+{
+    public class TicketsController : Controller
+    {
+        private readonly GestioncitasContext _context;
+
+        public TicketsController(GestioncitasContext context)
+        {
+            _context = context;
+        }
+
+        // Generar un ticket para un paciente
+        [HttpPost]
+        public async Task<IActionResult> Create(int pacienteId)
+        {
+            var ticket = new Ticket
+            {
+                PacienteId = pacienteId
+            };
+
+            _context.Tickets.Add(ticket);
+            await _context.SaveChangesAsync();
+
+            TempData["SuccessMessage"] = $"Ticket #{ticket.Id} generado exitosamente.";
+            return RedirectToAction("Index", "Pacientes");
+        }
+
+        // Reporte de tickets para recepcionistas
+        public async Task<IActionResult> Reporte()
+        {
+            var tickets = await _context.Tickets
+                .Include(t => t.Paciente)
+                .OrderBy(t => t.Id) // Orden por ID (orden de llegada)
+                .ToListAsync();
+
+            return View(tickets);
+        }
+    }
+}
---
+++ gestionCitas/Models/Ticket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace gestionCitas.Models
+{
+    public class Ticket
+    {
+        [Key]
+        public int Id { get; set; } // ID del ticket
+
+        [Required]
+        public int PacienteId { get; set; }
+
+        [ForeignKey("PacienteId")]
+        public Paciente Paciente { get; set; }
+
+        [Required]
+        public DateTime FechaCreacion { get; set; } = DateTime.Now;
+    }
+}
