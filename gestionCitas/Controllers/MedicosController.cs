1dc4e3cd087f117c240dde4a88663e480d1a9c8b
nicolealiendresistemas <nicolealiendresistemas@gmail.com>
2024-12-01 17:17:27 -0400

    cambios

--- gestionCitas/Controllers/MedicosController.cs
+++ gestionCitas/Controllers/MedicosController.cs
@@ -192,6 +192,29 @@ namespace gestionCitas.Controllers
 
             return PartialView("_MedicosPorEspecialidad", especialidade.Medicos);
         }
-    
+
+        public async Task<IActionResult> ReportePacientes(int? medicoId)
+        {
+            // Obtener la lista de médicos para el dropdown
+            ViewData["MedicoId"] = new SelectList(_context.Medicos, "Id", "Nombre");
+
+            // Si se seleccionó un médico, filtrar sus pacientes
+            if (medicoId.HasValue)
+            {
+                var pacientes = await _context.Citas
+                .Where(c => c.MedicoId == medicoId)
+                .Select(c => c.Paciente)
+                .ToListAsync();
+
+
+                ViewBag.SelectedMedicoId = medicoId.Value;
+                return View(pacientes);
+            }
+
+            // Si no se seleccionó ningún médico, mostrar una vista vacía
+            return View(new List<Paciente>());
+        }
+
+
     }
 }
