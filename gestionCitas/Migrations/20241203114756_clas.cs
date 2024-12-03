a7c4d73c22d91674f27b4f55103f975f8d259d49
nicolealiendresistemas <nicolealiendresistemas@gmail.com>
2024-12-03 08:18:36 -0400

    c

---
+++ gestionCitas/Migrations/20241203114756_clas.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace gestionCitas.Migrations
+{
+    /// <inheritdoc />
+    public partial class clas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
